fbc25330bd6dc10801658f7248389e9063f5f179
vitaliks <vitaly@anagorsky.com>
2019-03-09 00:11:01 +0300

    0.0.1

---
+++ sources/index.js
@@ -0,0 +1,494 @@
+
+/**
+ *
+ */
+
+const isUndefinedOrNull = (message) => {
+
+	/**
+	 *
+	 */
+
+	return message === undefined || message === null
+
+}
+
+/**
+ *
+ */
+
+const validate = (message, schema, up, field) => {
+
+	/**
+	 *
+	 */
+
+	if (!schema['$type']) {
+
+		/**
+		 *
+		 */
+
+		throw new Error('Each schema must contain the $type operator')
+
+		/**
+		 *
+		 */
+
+	} else if (schema['$type'] === Boolean) {
+
+		/**
+		 *
+		 */
+
+		if (isUndefinedOrNull(message)) {
+
+			/**
+			 *
+			 */
+
+			if (schema.hasOwnProperty('$default')) {
+
+				/**
+				 *
+				 */
+
+				up[field] = schema['$default']
+
+				/**
+				 *
+				 */
+
+				return true
+
+				/**
+				 *
+				 */
+
+			} else {
+
+				/**
+				 *
+				 */
+
+				return false
+
+			}
+
+		} else {
+
+			/**
+			 *
+			 */
+
+			return message.constructor === Boolean;
+
+		}
+
+	} else if (schema['$type'] === Number) {
+
+		/**
+		 *
+		 */
+
+		if (isUndefinedOrNull(message)) {
+
+			/**
+			 *
+			 */
+
+			if (schema.hasOwnProperty('$default')) {
+
+				/**
+				 *
+				 */
+
+				up[field] = schema['$default']
+
+				/**
+				 *
+				 */
+
+				return true
+
+				/**
+				 *
+				 */
+
+			} else {
+
+				/**
+				 *
+				 */
+
+				return false
+
+			}
+
+		} else {
+
+			/**
+			 *
+			 */
+
+			return message.constructor === Number;
+
+		}
+
+	} else if (schema['$type'] === String) {
+
+		/**
+		 *
+		 */
+
+		if (isUndefinedOrNull(message)) {
+
+			/**
+			 *
+			 */
+
+			if (schema.hasOwnProperty('$default')) {
+
+				/**
+				 *
+				 */
+
+				up[field] = schema['$default']
+
+				/**
+				 *
+				 */
+
+				return true
+
+				/**
+				 *
+				 */
+
+			} else {
+
+				/**
+				 *
+				 */
+
+				return false
+
+			}
+
+		} else {
+
+			/**
+			 *
+			 */
+
+			if (message.constructor === String) {
+
+				/**
+				 *
+				 */
+
+				if (schema['$test']) {
+
+					/**
+					 *
+					 */
+
+					if (schema['$test'].constructor === RegExp) {
+
+						/**
+						 *
+						 */
+
+						return schema['$test'].test(message)
+
+						/**
+						 *
+						 */
+
+					} else if (schema['$test'].constructor === Function) {
+
+						/**
+						 *
+						 */
+
+						return schema['$test'](message)
+
+					}
+
+				}
+
+				/**
+				 *
+				 */
+
+				return true
+
+				/**
+				 *
+				 */
+
+			} else {
+
+				/**
+				 *
+				 */
+
+				return false
+
+			}
+
+		}
+
+	} else if (schema['$type'] === Object) {
+
+		/**
+		 *
+		 */
+
+		if (schema['$schema']) {
+
+			/**
+			 *
+			 */
+
+			if (isUndefinedOrNull(message)) {
+
+				/**
+				 *
+				 */
+
+				if (schema.hasOwnProperty('$default')) {
+
+					/**
+					 *
+					 */
+
+					up[field] = schema['$default']
+
+					/**
+					 *
+					 */
+
+					return true
+
+					/**
+					 *
+					 */
+
+				} else {
+
+					/**
+					 *
+					 */
+
+					return false
+
+				}
+
+			} else {
+
+				/**
+				 *
+				 */
+
+				if (message.constructor === Object) {
+
+					/**
+					 *
+					 */
+
+					for (const field in schema['$schema']) {
+
+						/**
+						 *
+						 */
+
+						if (!schema['$schema'].hasOwnProperty(field)) {
+
+							/**
+							 *
+							 */
+
+							continue
+
+						}
+
+						/**
+						 *
+						 */
+
+						if (!validate(message[field], schema['$schema'][field], message, field)) {
+
+							/**
+							 *
+							 */
+
+							return false
+
+						}
+
+					}
+
+					/**
+					 *
+					 */
+
+					return true
+
+					/**
+					 *
+					 */
+
+				} else {
+
+					/**
+					 *
+					 */
+
+					return false
+
+				}
+
+			}
+
+		} else {
+
+			/**
+			 *
+			 */
+
+			return false
+
+		}
+
+	} else if (schema['$type'] === Array) {
+
+		/**
+		 *
+		 */
+
+		if (!schema['$of']) {
+
+			/**
+			 *
+			 */
+
+			throw new Error('Array schema must contain the $of iterator that represents schema of values')
+
+			/**
+			 *
+			 */
+
+		} else {
+
+			/**
+			 *
+			 */
+
+			if (isUndefinedOrNull(message)) {
+
+				/**
+				 *
+				 */
+
+				if (schema.hasOwnProperty('$default')) {
+
+					/**
+					 *
+					 */
+
+					up[field] = schema['$default']
+
+					/**
+					 *
+					 */
+
+					return true
+
+					/**
+					 *
+					 */
+
+				} else {
+
+					/**
+					 *
+					 */
+
+					return false
+
+				}
+
+			} else {
+
+				/**
+				 *
+				 */
+
+				if (message.constructor === Array) {
+
+					/**
+					 *
+					 */
+
+					for (let i = 0; i < message.length; i++) {
+
+						/**
+						 *
+						 */
+
+						if (!validate(message[i], schema['$of'])) {
+
+							/**
+							 *
+							 */
+
+							return false
+
+						}
+
+					}
+
+					/**
+					 *
+					 */
+
+					return true
+
+					/**
+					 *
+					 */
+
+				} else {
+
+					/**
+					 *
+					 */
+
+					return false
+
+				}
+
+			}
+
+		}
+
+	} else {
+
+		/**
+		 *
+		 */
+
+		throw new Error('Unknown schema type. (only Boolean, Number, String, Object and Array are supported)')
+
+	}
+
+}
+
+/**
+ *
+ */
+
+module.exports = (message, schema) => validate(message, schema)
+
+
