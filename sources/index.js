409e3c1b9f0a0af518d3c26773079b2d37a89a7c
vitaliks <vitaly@anagorsky.com>
2019-05-29 01:45:36 +0300

    0.0.1

--- sources/index.js
+++ sources/index.js
@@ -3,517 +3,111 @@
  *
  */
 
+const ___error = require(`jiu-jitsu-error`)
+
+/**
+ *
+ */
+
 const isUndefinedOrNull = (message) => {
-
-	/**
-	 *
-	 */
-
 	return message === undefined || message === null
-
 }
 
 /**
  *
  */
 
-const validate = (schema, message, up, field) => {
+const validate = (schema, message, parent, field) => {
 
 	/**
 	 *
 	 */
 
-	if (!schema['$type']) {
-
-		/**
-		 *
-		 */
-
-		throw new Error('Each schema must contain the $type operator')
-
-		/**
-		 *
-		 */
-
-	} else if (schema['$type'] === Boolean) {
-
-		/**
-		 *
-		 */
-
-		if (isUndefinedOrNull(message)) {
-
-			/**
-			 *
-			 */
-
-			if (schema.hasOwnProperty('$default')) {
-
-				/**
-				 *
-				 */
-
-				up[field] = schema['$default']
-
-				/**
-				 *
-				 */
-
-				return true
-
-				/**
-				 *
-				 */
-
-			} else {
-
-				/**
-				 *
-				 */
-
-				return false
-
-			}
-
-		} else {
-
-			/**
-			 *
-			 */
-
-			return message.constructor === Boolean;
-
-		}
-
-		/**
-		 *
-		 */
-
-	} else if (schema['$type'] === Number) {
-
-		/**
-		 *
-		 */
-
-		if (isUndefinedOrNull(message)) {
-
-			/**
-			 *
-			 */
-
-			if (schema.hasOwnProperty('$default')) {
-
-				/**
-				 *
-				 */
-
-				up[field] = schema['$default']
-
-				/**
-				 *
-				 */
-
-				return true
-
-				/**
-				 *
-				 */
-
-			} else {
-
-				/**
-				 *
-				 */
-
-				return false
-
-			}
-
-			/**
-			 *
-			 */
-
-		} else {
-
-			/**
-			 *
-			 */
-
-			return message.constructor === Number;
-
-		}
-
-		/**
-		 *
-		 */
-
-	} else if (schema['$type'] === String) {
-
-		/**
-		 *
-		 */
-
-		if (isUndefinedOrNull(message)) {
-
-			/**
-			 *
-			 */
-
-			if (schema.hasOwnProperty('$default')) {
-
-				/**
-				 *
-				 */
-
-				up[field] = schema['$default']
-
-				/**
-				 *
-				 */
-
-				return true
-
-				/**
-				 *
-				 */
-
-			} else {
-
-				/**
-				 *
-				 */
-
-				return false
-
-			}
-
-			/**
-			 *
-			 */
-
-		} else {
-
-			/**
-			 *
-			 */
-
-			if (message.constructor === String) {
-
-				/**
-				 *
-				 */
-
-				if (schema['$test']) {
-
-					/**
-					 *
-					 */
-
-					if (schema['$test'].constructor === RegExp) {
-
-						/**
-						 *
-						 */
-
-						return schema['$test'].test(message)
-
-						/**
-						 *
-						 */
-
-					} else if (schema['$test'].constructor === Function) {
-
-						/**
-						 *
-						 */
-
-						return schema['$test'](message)
-
-					}
-
-				}
-
-				/**
-				 *
-				 */
-
-				return true
-
-				/**
-				 *
-				 */
-
-			} else {
-
-				/**
-				 *
-				 */
-
-				return false
-
-			}
-
-		}
-
-		/**
-		 *
-		 */
-
-	} else if (schema['$type'] === Object) {
-
-		/**
-		 *
-		 */
-
-		if (schema['$schema']) {
-
-			/**
-			 *
-			 */
-
-			if (isUndefinedOrNull(message)) {
-
-				/**
-				 *
-				 */
-
-				if (schema.hasOwnProperty('$default')) {
-
-					/**
-					 *
-					 */
-
-					up[field] = schema['$default']
-
-					/**
-					 *
-					 */
-
-					return true
-
-					/**
-					 *
-					 */
-
-				} else {
-
-					/**
-					 *
-					 */
-
-					return false
-
-				}
-
-				/**
-				 *
-				 */
-
-			} else {
-
-				/**
-				 *
-				 */
-
-				if (message.constructor === Object) {
-
-					/**
-					 *
-					 */
-
-					for (const field in schema['$schema']) {
-
-						/**
-						 *
-						 */
-
-						if (!schema['$schema'].hasOwnProperty(field)) {
-
-							/**
-							 *
-							 */
-
-							continue
-
-						}
-
-						/**
-						 *
-						 */
-
-						if (!validate(schema['$schema'][field], message[field], message, field)) {
-
-							/**
-							 *
-							 */
-
-							return false
-
-						}
-
-					}
-
-					/**
-					 *
-					 */
-
-					return true
-
-					/**
-					 *
-					 */
-
-				} else {
-
-					/**
-					 *
-					 */
-
-					return false
-
-				}
-
-			}
-
-			/**
-			 *
-			 */
-
-		} else {
-
-			/**
-			 *
-			 */
-
-			return false
-
-		}
-
-		/**
-		 *
-		 */
-
-	} else if (schema['$type'] === Array) {
-
-		/**
-		 *
-		 */
-
-		if (!schema['$of']) {
-
-			/**
-			 *
-			 */
-
-			throw new Error('Array schema must contain the $of iterator that represents schema of values')
-
-			/**
-			 *
-			 */
-
-		} else {
-
-			/**
-			 *
-			 */
-
-			if (isUndefinedOrNull(message)) {
-
-				/**
-				 *
-				 */
-
-				if (schema.hasOwnProperty('$default')) {
-
-					/**
-					 *
-					 */
-
-					up[field] = schema['$default']
-
-					/**
-					 *
-					 */
-
-					return true
-
-					/**
-					 *
-					 */
-
-				} else {
-
-					/**
-					 *
-					 */
-
-					return false
-
-				}
-
-			} else {
-
-				/**
-				 *
-				 */
-
-				if (message.constructor === Array) {
-
-					/**
-					 *
-					 */
-
-					for (let i = 0; i < message.length; i++) {
-
-						/**
-						 *
-						 */
-
-						if (!validate(schema['$of'], message[i])) {
-
-							/**
-							 *
-							 */
-
-							return false
-
-						}
-
-					}
-
-					/**
-					 *
-					 */
-
-					return true
-
-					/**
-					 *
-					 */
-
-				} else {
-
-					/**
-					 *
-					 */
-
-					return false
-
-				}
-
-			}
-
-		}
-
-	} else {
-
-		/**
-		 *
-		 */
-
-		throw new Error('Unknown schema type. (only Boolean, Number, String, Object and Array are supported)')
-
+	if (!schema[`$type`]) {
+		throw new Error(`Each schema must contain the $type operator`)
 	}
+
+	/**
+	 *
+	 */
+
+	if (schema[`$type`] === Boolean) {
+		if (isUndefinedOrNull(message)) {
+			const hasDefault = schema.hasOwnProperty(`$default`)
+			parent[field] = hasDefault ? schema[`$default`] : undefined
+			return hasDefault
+		} else {
+			return message.constructor === Boolean
+		}
+	}
+
+	/**
+	 *
+	 */
+
+	if (schema[`$type`] === Number) {
+		if (isUndefinedOrNull(message)) {
+			const hasDefault = schema.hasOwnProperty(`$default`)
+			parent[field] = hasDefault ? schema[`$default`] : undefined
+			return hasDefault
+		} else {
+			return message.constructor === Number
+		}
+	}
+
+	/**
+	 *
+	 */
+
+	if (schema[`$type`] === String) {
+		if (isUndefinedOrNull(message)) {
+			const hasDefault = schema.hasOwnProperty(`$default`)
+			parent[field] = hasDefault ? schema[`$default`] : undefined
+			return hasDefault
+		} else {
+			return message.constructor === String && (schema[`$test`] ? schema[`$test`](message) : true)
+		}
+	}
+
+	/**
+	 *
+	 */
+
+	if (schema[`$type`] === Object) {
+		if (schema[`$schema`]) {
+			if (isUndefinedOrNull(message)) {
+				const hasDefault = schema.hasOwnProperty(`$default`)
+				parent[field] = hasDefault ? schema[`$default`] : undefined
+				return hasDefault
+			} else {
+				return message.constructor === Object && Object.keys(schema[`$schema`]).every(($field) => validate(schema[`$schema`][$field], message[$field], message, $field))
+			}
+		}
+	}
+
+	/**
+	 *
+	 */
+
+	if (schema[`$type`] === Array) {
+		if (isUndefinedOrNull(message)) {
+			const hasDefault = schema.hasOwnProperty(`$default`)
+			parent[field] = hasDefault ? schema[`$default`] : undefined
+			return hasDefault
+		} else {
+			if (!schema[`$of`]) {
+				throw ___error(`Array schema must contain the $of iterator that represents schema of values`)
+			} else {
+				return message.constructor === Array && Object.keys(schema[`$schema`]).every(($item) => validate(schema[`$of`], $item))
+			}
+		}
+	}
+
+	/**
+	 *
+	 */
+
+	throw ___error(`Unknown schema type. (Boolean, Number, String, Object and Array are supported only)`)
 
 }
 
